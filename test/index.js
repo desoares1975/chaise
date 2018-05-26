6dfb24afc831fa154d1237bad21d7c9fba5853d8
desoares <fabio.desoares@gmail.com>
2018-05-25 22:52:36 -0300

    new aproach

--- test/index.js
+++ test/index.js
@@ -1,1 +1,21 @@
-'use strict';
+'use strict'; 
+
+const request = require('request');
+const { expect } = require('chai');
+const chaise = require('../');
+
+describe('Chaise testing', () => {
+  it('Gets an instance of Chaise', done => {
+    expect(chaise).to.have.own.property('Schema');
+    expect(chaise).to.have.property('connect');
+    expect(chaise).to.have.property('authenticate');
+    done();
+  });
+  it('test connection', done => {
+    chaise.connect({})
+      .then(() => {
+        done();
+      })
+      .catch(e => done(e));
+  });
+});
